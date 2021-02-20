1baefe683d4dee818439db28eca26bded11b49e9
Worthy-Wang <825622809@qq.com>
2021-02-20 22:13:25 +0800

    update

--- Tree/AVL/main.cpp
+++ Tree/AVL/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 
 /*********************************AVL(平衡二叉树)**************************************/
@@ -17,9 +18,9 @@ struct TreeNode
 class AVLTree
 {
     TreeNode *_root;
-
 public:
-    AVLTree(const char *);
+    AVLTree();
+    AVLTree(string s);
     ~AVLTree();
 
     void preTraverse(TreeNode *);              //前序遍历
@@ -28,6 +29,8 @@ public:
     TreeNode *getRoot() const { return _root; }
     TreeNode* insertNode(TreeNode *, int val);         //插入节点操作
     TreeNode *deleteNode(TreeNode *, int val); //删除操作
+    void insertNode(int val);         //插入节点操作
+    void deleteNode(int val); //删除操作
 
     //AVL树特有的成员变量
     TreeNode *RR(TreeNode *);    //左旋
@@ -41,12 +44,20 @@ private:
 
 /*********************************类成员定义**************************************/
 
-AVLTree::AVLTree(const char *str)
+AVLTree::AVLTree()
+:_root(nullptr)
+{
+    cout << "AVLTree()" << endl;
+}
+
+AVLTree::AVLTree(string s)
     : _root(NULL)
 {
     cout << "AVLTree()" << endl;
-    for (int i = 0; str[i] != '\0'; i++)
-        _root = insertNode(_root, str[i] - 48);
+    stringstream ss(s);
+    int val;
+    while (ss >> val)
+        _root = insertNode(_root, val);
 }
 
 AVLTree::~AVLTree()
@@ -75,7 +86,7 @@ void AVLTree::preTraverse(TreeNode *TreeNode)
 {
     if (TreeNode)
     {
-        cout << TreeNode->val;
+        cout << TreeNode->val << " ";
         preTraverse(TreeNode->left);
         preTraverse(TreeNode->right);
     }
@@ -86,7 +97,7 @@ void AVLTree::inTraverse(TreeNode *TreeNode)
     if (TreeNode)
     {
         inTraverse(TreeNode->left);
-        cout << TreeNode->val;
+        cout << TreeNode->val << " ";
         inTraverse(TreeNode->right);
     }
 }
@@ -145,37 +156,119 @@ TreeNode* AVLTree::RL(TreeNode* root)
 }
 
 
-TreeNode* AVLTree::insertNode(TreeNode *p, int val)
+TreeNode* AVLTree::insertNode(TreeNode *root, int val)
 {
-    if (!p)
+    if (!root)
     {
         TreeNode* node = new TreeNode(val);
         return node;    
     }
-    if (p->val == val)
+
+    if (root->val == val)
     {
-        return p; //AVL树中已经有该节点，不能够再插入
+        return root; //AVL树中已经有该节点，不能够再插入
     }
-    else if (p->val < val)
+    else if (val > root->val)
     {
         //向右插入
-        p->right = insertNode(p->right, val);
+        root->right = insertNode(root->right, val);
         //插入新节点后如果失去平衡则调整
-        if ()
-    }else if (p->val > val)
+        if (getHeight(root->right) - getHeight(root->left) == 2)
+        {
+            TreeNode* t = root->right;
+            if (getHeight(t->left) > getHeight(t->right))
+                root = RL(root);
+            else
+                root = RR(root);
+        }
+    }else if (val < root->val)
     {
         //向左插入
-        p->left = insertNode(p->left, val);
+        root->left = insertNode(root->left, val);
         //插入新节点后如果失去平衡则调整
-        
+        if (getHeight(root->left) - getHeight(root->right) == 2)
+        {
+            TreeNode* t = root->left;
+            if (getHeight(t->left) > getHeight(t->right))
+                root = LL(root);
+            else
+                root = LR(root);
+        }
     }
-    return p;
+    root->height = std::max(getHeight(root->left), getHeight(root->right)) + 1;
+    return root;
 }
 
+void AVLTree::insertNode(int val)
+{
+    _root  = insertNode(_root, val);
+}
+
+void AVLTree::deleteNode(int val)
+{
+    _root = deleteNode(_root, val);
+}
 
 TreeNode *AVLTree::deleteNode(TreeNode *root, int key)
 {
+    if (!root)//该节点并不存在
+        return nullptr;
 
+    if (key < root->val)
+    {
+        //key在root左侧
+        root->left = deleteNode(root->left, key);
+        if (getHeight(root->right) - getHeight(root->left) == 2)
+        {
+            TreeNode* t = root->right;
+            if (getHeight(t->right) > getHeight(t->left))
+                root = RR(root);
+            else
+                root = RL(root);
+        }
+    }else if (key > root->val)
+    {
+        //key在root右侧
+        root->right = deleteNode(root->right, key);
+        if (getHeight(root->left) - getHeight(root->right) == 2)
+        {
+            TreeNode* t = root->left;
+            if (getHeight(t->left) > getHeight(t->right))
+                root = LL(root);
+            else
+                root = LR(root);
+        }
+    }else
+    {
+        //找到要删除的节点key
+        if (root->left && root->right)
+        {
+            if (getHeight(root->left) > getHeight(root->right))//从更高的子树开始删除，可以保证删除之后AVL仍然平衡
+            {
+                TreeNode* t = root->left;
+                while(t->right)
+                    t = t->right;
+                root->val = t->val;
+                root->left = deleteNode(root->left, t->val);
+            }else
+            {
+                TreeNode* t = root->right;
+                while(t->left)
+                    t = t->left;
+                root->val = t->val;
+                root->right = deleteNode(root->right, t->val);
+            }
+        }else
+        {
+            TreeNode* t = root;
+            root = root->left ? root->left : root->right;
+            delete t;
+            return root;
+        }
+    }
+
+    root->height = std::max(getHeight(root->left), getHeight(root->right)) + 1;
+    return root;
 }
 
 
@@ -184,16 +277,36 @@ TreeNode *AVLTree::deleteNode(TreeNode *root, int key)
 /**************************************测试函数****************************************/
 void test1()
 {
-    AVLTree tree("3265879");
+    AVLTree tree;
+    tree.insertNode(3);
+    // tree.deleteNode(3);
+}
+
+void test2()
+{
+    AVLTree tree("3 2 1 4 5 6 7 16 15 14 13 12 11 10 8 9");
     tree.preTraverse(tree.getRoot());
     cout << endl;
     tree.inTraverse(tree.getRoot());
     cout << endl;
     cout << "Height:" << tree.getHeight(tree.getRoot()) << endl;
+
+}
+
+void test3()
+{
+    AVLTree tree("3 2 1 4 5 6 7 16 15 14 13 12 11 10 8 9");
+    tree.deleteNode(8);
+    tree.preTraverse(tree.getRoot());
+    cout << endl;
+    tree.inTraverse(tree.getRoot());
+    cout << endl;
+    cout << "Height:" << tree.getHeight(tree.getRoot()) << endl;
+
 }
 
 int main()
 {
-    test1();
+    test3();
     return 0;
 }
